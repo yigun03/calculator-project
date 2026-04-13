3f60ed75e12cdd58cf037b3ce8aae0885c2271e4
kwon <t2024-m0216@t2024-m0216ui-MacBookAir.local>
2026-04-13 11:24:48 +0900

    create def subtract

--- calculator.py
+++ calculator.py
@@ -3,3 +3,9 @@ def add(a, b):
     Args: a, b (float): 더할 두 수
     Returns: float: a + b"""
     return a + b
+
+def subtract(a, b):
+    """두 수의 차를 반환합니다.
+    Args: a, b (float): 첫 번째 수에서 두 번째 수를 뺩니다
+    Returns: float: a - b"""
+    return a - b
